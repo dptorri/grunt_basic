d98fc041f4bab867ad73f71cd6537354db6c8728
Daniel Palacios <dptorri@gmail.com>
2017-12-12 22:35:50 +0100

    from warning to fatal error
    in this way you cannot --force operations

--- first_task/Gruntfile.js
+++ first_task/Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt){
     grunt.registerTask('add_num', function(first, second){
     var result = Number(first) + Number(second);
         if(isNaN(Number(first)) || isNaN(Number(second)) ){
-            grunt.warn('Both parameters must be numbers.');
+            grunt.fatal('Both parameters must be numbers.');
             }
 
     grunt.log.writeln(first + ' + ' + second + ' = ' + result);
