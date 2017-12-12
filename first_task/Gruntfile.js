11a1ba3d3deca4fb91889e033b1ad1bda5c98c24
Daniel Palacios <dptorri@gmail.com>
2017-12-12 22:27:23 +0100

    Throwing_exceptions
    if any is NaN throws an exception

--- first_task/Gruntfile.js
+++ first_task/Gruntfile.js
@@ -2,6 +2,10 @@ module.exports = function(grunt){
 
     grunt.registerTask('add_num', function(first, second){
     var result = Number(first) + Number(second);
+        if(isNaN(Number(first)) || isNaN(Number(second)) ){
+            grunt.warn('Both parameters must be numbers.');
+            }
+
     grunt.log.writeln(first + ' + ' + second + ' = ' + result);
     });
     }
