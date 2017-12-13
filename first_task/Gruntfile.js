a8f6301b58cca0a598081c1fb465f28f8cc7caff
Daniel Palacios <dptorri@gmail.com>
2017-12-13 05:39:59 +0100

    channing all at once

--- first_task/Gruntfile.js
+++ first_task/Gruntfile.js
@@ -1,11 +1,14 @@
 module.exports = function(grunt){
 
     grunt.registerTask('add_num', function(first, second){
-    var result = Number(first) + Number(second);
-        if(isNaN(Number(first)) || isNaN(Number(second)) ){
-            grunt.fatal('Both parameters must be numbers.');
-            }
-
-    grunt.log.writeln(first + ' + ' + second + ' = ' + result);
+        var result = Number(first) + Number(second);
+            if(isNaN(Number(first)) || isNaN(Number(second)) ){
+                grunt.fatal('Both parameters must be numbers.');
+                }
+            grunt.log.writeln(first + ' + ' + second + ' = ' + result);
     });
+    grunt.registerTask('say_hi', function(name){
+        grunt.log.writeln('Hi '+name+' good to meet you!');
+    });
+        grunt.registerTask('default', ['say_hi:Daniel', 'add_num:2:3']);
     }
