800440edc90f47331487fa93f93f0b3423bb9e04
Daniel Palacios <dptorri@gmail.com>
2017-12-13 05:44:18 +0100

    tasks have comments

--- first_task/Gruntfile.js
+++ first_task/Gruntfile.js
@@ -1,5 +1,5 @@
 module.exports = function(grunt){
-
+//Task: add_num ; adds 2 numbers and handles NaN cases
     grunt.registerTask('add_num', function(first, second){
         var result = Number(first) + Number(second);
             if(isNaN(Number(first)) || isNaN(Number(second)) ){
@@ -7,8 +7,11 @@ module.exports = function(grunt){
                 }
             grunt.log.writeln(first + ' + ' + second + ' = ' + result);
     });
+//Task: say_hi ; says Hi given a variable $grunt say_hi:Daniel
     grunt.registerTask('say_hi', function(name){
         grunt.log.writeln('Hi '+name+' good to meet you!');
     });
+//Task: chained unnamed task displays say_hi and add_num
         grunt.registerTask('default', ['say_hi:Daniel', 'add_num:2:3']);
+    
     }
