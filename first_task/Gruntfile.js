ba170e1be204c7c538cdee5913c0bfad45420ade
Daniel Palacios <dptorri@gmail.com>
2017-12-13 06:21:03 +0100

    description added

--- first_task/Gruntfile.js
+++ first_task/Gruntfile.js
@@ -1,10 +1,13 @@
 module.exports = function(grunt){
 
 grunt.registerTask('temps_F_C','converts °F to °C and °C to °F for a number given.', function(num){
+    var description = 'description fo the task: temps_F_C converts(num) °F to °C and °C to °F for a number given.';
     var result = Number(num);
-    
-    grunt.log.writeln('If you enter a °C temperature of '+result+'the °F value would be'+((result*9)/5)+32);
-    grunt.log.writeln('but if '+result+'is in °F, then the value in °C would be'+((result-32)*5)/9);
+    var result_c = (((result-32)*5)/9);
+    var result_f = (((result*9)/5)+32);
+    grunt.log.writeln(description);
+    grunt.log.writeln('If you entered a  temperature of '+result+'°C then the °F value would be '+result_f+'°F');
+    grunt.log.writeln(' but if '+result+' is in °F, then the value in °C would be '+result_c+'°C');
  });
 }
  
